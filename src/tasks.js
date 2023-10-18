c2b61bf0a046924bea733db74283c1bba2232d84
Vidoalessandro <gabriellevido17@gmail.com>
2023-10-17 21:28:27 -0400

    Tasks list file

---
+++ src/tasks.js
@@ -0,0 +1,32 @@
+export const tasks = [
+    {
+        id: 0,
+        title: 'Task 1',
+        description: 'My first task'
+    },
+    {
+        id: 1,
+        title: 'Task 2',
+        description: 'My second task'
+    },
+    {
+        id: 2,
+        title: 'Task 3',
+        description: 'My third task'
+    },
+    {
+        id: 3,
+        title: 'Task 4',
+        description: 'My fourth task'
+    },
+    {
+        id: 4,
+        title: 'Task 5',
+        description: 'My fifth task'
+    },
+    {
+        id: 5,
+        title: 'Task 6',
+        description: 'My sixth task'
+    },
+];
