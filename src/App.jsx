deb463634d9db0c61b48237b6d7ef0dc1d9dad9f
Vidoalessandro <gabriellevido17@gmail.com>
2023-10-17 22:21:52 -0400

    Add tasks to the form

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,29 @@
+import { tasks as data } from "./tasks";
+import {useState, useEffect} from 'react'
 import TaskList from './TaskList'
+import TaskForm from './TaskForm'
 
 function App() {
+
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    setTasks(data)
+  }, [])
+
+  const createTask = (taskTitle) => {
+    setTasks([...tasks, {
+      title: taskTitle,
+      id: tasks.length,
+      description: 'New task',
+    }])
+  }
+
   return (
-    <div>
-      <TaskList />
-    </div>
+    <>
+      <TaskForm createTask={createTask}/>
+      <TaskList tasks={tasks} />
+    </>
   )
 }
 
