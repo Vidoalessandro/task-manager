c2e52636c4e8f42279334186dc83839734533287
Vidoalessandro <gabriellevido17@gmail.com>
2023-10-20 21:50:50 -0400

    useContext and styles

--- src/components/TaskForm.jsx
+++ src/components/TaskForm.jsx
@@ -1,8 +1,10 @@
-import { useState } from "react";
+import { useState, useContext } from "react";
+import { TaskContext } from "../context/TaskContext";
 
-function TaskForm({ createTask }) {
+function TaskForm() {
   const [title, setTitle] = useState("");
   const [description, setDescription] = useState("");
+  const { createTask } = useContext(TaskContext);
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -16,30 +18,37 @@ function TaskForm({ createTask }) {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <input
-        placeholder="Write your task"
-        type="text"
-        name=""
-        id=""
-        onChange={(e) => {
-          setTitle(e.target.value);
-        }}
-        value={title}
-        autoFocus
-      />
-      <textarea
-        placeholder="Write the task description"
-        name=""
-        id=""
-        cols="30"
-        rows="10"
-        onChange={(e) => {
-          setDescription(e.target.value);
-        }}
-        value={description}
-      ></textarea>
-      <button>Save</button>
+    <form onSubmit={handleSubmit} className="bg-slate-800 p-10 mb-4">
+      <div className="max-w-md mx-auto">
+        <h1 className="text-white text-2xl font-bold mb-3">Create your task</h1>
+        <input
+          className="bg-slate-300 p-3 w-full mb-2"
+          placeholder="Write your task"
+          type="text"
+          name=""
+          id=""
+          onChange={(e) => {
+            setTitle(e.target.value);
+          }}
+          value={title}
+          autoFocus
+        />
+        <textarea
+          className="bg-slate-300 p-3 w-full mb-2 h-12"
+          placeholder="Write the task description"
+          name=""
+          id=""
+          cols="30"
+          rows="10"
+          onChange={(e) => {
+            setDescription(e.target.value);
+          }}
+          value={description}
+        ></textarea>
+        <button className="bg-indigo-500 px-3 py-1 text-white rounded-md">
+          Save
+        </button>
+      </div>
     </form>
   );
 }
--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -1,14 +1,22 @@
 import TaskCard from "./TaskCard";
+import { TaskContext } from "../context/TaskContext";
+import { useContext } from "react";
 
-function TaskList({ tasks, deleteTask }) {
+function TaskList() {
+  const { tasks } = useContext(TaskContext);
+
   if (tasks.length === 0) {
-    return <h1>There isnt tasks yet</h1>;
+    return (
+      <h1 className="text-white text-2xl font-bold mb-3 text-center">
+        There isnt tasks yet
+      </h1>
+    );
   }
 
   return (
-    <div>
+    <div className="grid grid-cols-4 gap-2">
       {tasks.map((task) => (
-        <TaskCard task={task} deleteTask={deleteTask} key={task.id} />
+        <TaskCard task={task} key={task.id} />
       ))}
     </div>
   );
